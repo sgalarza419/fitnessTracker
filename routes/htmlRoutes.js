651b6bc8047dc477e84d4f1f23dab4d199b9f85c
S Galarza <sgalarza4190@gmail.com>
2020-12-20 22:20:10 -0500

    npm i

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,9 +5,11 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/index.html"))
     });
 
-    app.get("/stats", function(req,res){
+    app.get("/stats", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/stats.html"))
     });
 
-    
+    app.get("/exercise", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"))
+    });
 };
