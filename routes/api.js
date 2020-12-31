514a579be4ba51c5dd8238fa47d4db0335f5843a
S Galarza <sgalarza4190@gmail.com>
2020-12-30 21:23:55 -0500

    started populated api.js file with api routes

--- routes/api.js
+++ routes/api.js
@@ -1,50 +1,16 @@
-  const API = {
-    async getLastWorkout() {
-      let res;
-      try {
-        res = await fetch("/api/workouts");
-      } catch (err) {
-        console.log(err)
-      }
-      const json = await res.json();
+const router = require("express").Router();
+const {
+  workout
+} = require("../models");
 
-      return json[json.length - 1];
-    },
-    async addExercise(data) {
-      const id = location.search.split("=")[1];
-
-      const res = await fetch("/api/workouts/" + id, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-      });
-
-      const json = await res.json();
-
-      return json;
-    },
-    async createWorkout(data = {}) {
-      const res = await fetch("/api/workouts", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-
-      const json = await res.json();
-
-      return json;
-    },
-
-    async getWorkoutsInRange() {
-      const res = await fetch(`/api/workouts/range`);
-      const json = await res.json();
-
-      return json;
-    },
-  };
-
-module.exports = API;
+router.post("/api/workouts", ({
+  body
+}, res) => {
+  workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
